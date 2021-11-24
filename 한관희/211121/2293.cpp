923cd467a330edc2d4387d648ed9ae4f249c6c50
Lime <noop103@naver.com>
2021-11-24 15:54:08 +0900

    첫주차 스터디내용 업로드

---
+++ 한관희/211121/2293.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    vector<int> dp(k + 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    dp[0] = 1;
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = v[i]; j <= k; j++) {
+            dp[j] += dp[j - v[i]];
+        }
+    }
+
+    cout << dp[k] << "\n";
+
+    return 0;
+}
