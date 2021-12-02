cb10e4a60579af6d004ddce8b547fea4bb599cbc
Lime <noop103@naver.com>
2021-12-02 14:59:47 +0900

    3079 추가

--- 동적계획법/2293_HGH.cpp
+++ 동적계획법/2293_HGH.cpp
@@ -8,7 +8,6 @@ int main()
     ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    freopen("input.txt", "rt", stdin);
 
     int n, k;
     cin >> n >> k;
---
+++ 이분탐색/3079_HGH.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int main()
+{
+    int n, m;
+    long long int max = 0;
+    long long int* k;
+    
+    scanf("%d %d", &n, &m);
+    k = (long long int*)malloc(n * sizeof(long long int));
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &k[i]);
+        max = MAX(max, k[i]);
+    }
+
+    long long int left = 1, right = m * max;
+    long long int res = 0;
+
+    while (left <= right) {
+        long long int mid = (left + right) / 2;
+        long long int pass = 0;
+
+        for (int i = 0; i < n; i++) {
+            pass += mid / k[i];
+        }
+
+        if (pass < m) left = mid + 1;
+        else {
+            res = mid;
+            right = mid - 1;
+        }
+    }
+    printf("%lld", res);
+
+    free(k);
+
+    return 0;
+}
