0ea50d7c4db056e16b837f4852a5477f5263ced2
Lime <noop103@naver.com>
2022-02-15 16:23:21 +0900

    한관희 1764 업로드

---
+++ 해시/1764_HGH.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, m, cnt = 0;
+    vector<string> vec;
+    vector<string> res;
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        string tmp;
+        cin >> tmp;
+        vec.push_back(tmp);
+    }
+    sort(vec.begin(), vec.end());
+
+    for (int i = 0; i < m; i++) {
+        string tmp;
+        cin >> tmp;
+        if (binary_search(vec.begin(), vec.end(), tmp)) {
+            cnt++;
+            res.push_back(tmp);
+        }
+    }
+
+    sort(res.begin(), res.end());
+    cout << cnt << endl;
+    for (string elem : res) {
+        cout << elem << endl;
+    }
+
+    return 0;
+}
