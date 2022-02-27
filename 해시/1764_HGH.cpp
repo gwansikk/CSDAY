3077eeaad8b2cc5fccd0c1da07d8196cbb86fbce
Lime <noop103@naver.com>
2022-02-27 22:14:27 +0900

    1764 수정

--- 해시/1764_HGH.cpp
+++ 해시/1764_HGH.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 int main()
 {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n, m, cnt = 0;
     vector<string> vec;
     vector<string> res;
